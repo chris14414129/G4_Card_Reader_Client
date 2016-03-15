6b9a3c9d8d0dd11e323dd7c4986e4e81daada7dd
“chris” <chris@email>
2016-03-15 10:40:25 +0000

    communication format comment added

--- Card_Reader_Client/src/CardReaderClient.java
+++ Card_Reader_Client/src/CardReaderClient.java
@@ -7,6 +7,39 @@ import javax.swing.border.LineBorder;
 import javax.swing.event.DocumentListener;
 import java.util.Scanner;
 
+/* Commands for communication
+ * XXXYYYREST
+ * XXX == room_id e.g. 101
+ * YYY == function e.g. UPD
+ * REST == data
+ * 
+ * Receiving
+ * UPD == Update display for session, - for demo purposes only, wont be in string
+ * REST == Sessioncode(A)-Sessionname(B)-Starttime(C)-Endtime(d)
+ * A== String 11
+ * B== String 30
+ * C== String 5
+ * D== String 5
+ * 
+ * Example:
+ * 101UPDCSY1234/01Test Session                  10:0011:00
+ * 
+ * CON == confirmation of student registration
+ * REST == String 4
+ * PASS
+ * FAIL
+ * 
+ * e.g
+ * 101CONPASS
+ * 
+ * Sending
+ * REG == student registration
+ * REST == int 8 student_id
+ * 
+ * e.g.
+ * 101REG12345678
+ */
+
 
 
 public class CardReaderClient extends JFrame {
